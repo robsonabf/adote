372abcb9f375e426b307e7d8285544782b568a96
robsonabf <32685128+robsonabf@users.noreply.github.com>
2024-05-23 16:43:38 -0300

    hahaha

--- adote/adote/settings.py
+++ adote/adote/settings.py
@@ -148,9 +148,12 @@ LOGIN_URL = '/login/'
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 # Redireciona todas as solicitações HTTP para HTTPS
-"""SECURE_SSL_REDIRECT = True
+""" 
+SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True"""
+SECURE_HSTS_PRELOAD = True 
+
+"""
